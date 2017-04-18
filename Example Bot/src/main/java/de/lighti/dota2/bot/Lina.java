8c387625d5feecda408b8be9291bb85a815f08e6
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2017-04-17 23:49:02 -0400

    Added tensorflow to maven project and added basic testing to Lina constructor

--- Example Bot/src/main/java/de/lighti/dota2/bot/Lina.java
+++ Example Bot/src/main/java/de/lighti/dota2/bot/Lina.java
@@ -1,6 +1,12 @@
 package de.lighti.dota2.bot;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Random;
+        
+import org.tensorflow.Graph;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+import org.tensorflow.TensorFlow;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -49,6 +55,31 @@ public class Lina extends BaseBot {
         System.out.println( "Creating Lina" );
         myLevels = new int[5];
 
+        // test tensorflow
+        try (Graph g = new Graph()) {
+            final String value = "Hello from " + TensorFlow.version();
+
+            // Construct the computation graph with a single operation, a constant
+            // named "MyConst" with a value "value".
+            try (Tensor t = Tensor.create(value.getBytes("UTF-8"))) {
+              // The Java API doesn't yet include convenience functions for adding operations.
+              g.opBuilder("Const", "MyConst").setAttr("dtype", t.dataType()).setAttr("value", t).build();
+            }
+            catch (UnsupportedEncodingException e) {
+            	e.printStackTrace();
+            	System.exit(1);
+            }
+
+            // Execute the "MyConst" operation in a Session.
+            try (Session s = new Session(g);
+                 Tensor output = s.runner().fetch("MyConst").run().get(0)) {
+              System.out.println(new String(output.bytesValue(), "UTF-8"));
+            }
+            catch (UnsupportedEncodingException e) {
+            	e.printStackTrace();
+            	System.exit(1);
+            }
+		}
     }
 
     @Override
