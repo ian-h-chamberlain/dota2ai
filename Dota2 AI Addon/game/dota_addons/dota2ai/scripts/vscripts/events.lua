07cdd3c2e2d854660906fd700c9fdac6ef235686
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2017-05-09 23:06:27 -0400

    Automatically start a new game when the current one ends

--- Dota2 AI Addon/game/dota_addons/dota2ai/scripts/vscripts/events.lua
+++ Dota2 AI Addon/game/dota_addons/dota2ai/scripts/vscripts/events.lua
@@ -17,6 +17,9 @@ function Dota2AI:OnGameRulesStateChange()
   elseif nNewState == DOTA_GAMERULES_STATE_PRE_GAME then
     print( "OnGameRulesStateChange: Pre Game Selection" )
     SendToServerConsole( "dota_dev forcegamestart" ) -- Skip the draft process
+  elseif nNewState == DOTA_GAMERULES_STATE_POST_GAME then
+    print( "OnGameRulesStateChange: Game Ended")
+	SendToServerConsole( "dota_launch_custom_game dota2ai dota")
   elseif nNewState == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
     print( "OnGameRulesStateChange: Game In Progress" )
 	BotPick()
