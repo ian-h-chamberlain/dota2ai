6345d7cce5e35ec991aaeac837d6b8a9e3672685
Connor Fahy <heskey333@gmail.com>
2017-05-09 21:36:02 -0400

    Fixed a null reference

--- Example Bot/src/main/java/de/lighti/dota2/bot/AgentData.java
+++ Example Bot/src/main/java/de/lighti/dota2/bot/AgentData.java
@@ -231,7 +231,7 @@ public class AgentData {
 		reward += 10 * (agent.getGold() - gold);
 		reward += agent.getHealth() - hp;
 		if(agent.getHealth() > agent.getMaxHealth()/2){
-			reward += Vec3.distance(pos, new float[]{-8000,-8000})/ 16000;
+			reward += Vec3.distance(agent.getOrigin(), new float[]{-8000,-8000})/ 16000;
 		}
 		//reward += 
 		return reward;
